af591937c217e4f9ed6932f6d4256b3e5249d823
Brysongathuku <brysongathuku189@gmail.com>
2024-09-27 11:57:24 +0300

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
  import './Navbar.css'
 import logo from '../../assets/logo.png'
 import { useState,useEffect } from 'react';
+import { Link } from 'react-scroll'; 
 
 
 function Navbar() {
@@ -15,12 +16,22 @@ useEffect(()=>{
       <img src={logo} alt="" className='logo'/>
       <ul>
        
-        <li>Home </li>
-        <li>program</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li><button className='btn'>Contact us</button></li>
+        <li>
+           <Link to='hero' smooth ={true} offset= {0} duration={500}>Home</Link>
+           </li>
+        <li>
+        <Link to='program' smooth ={true} offset= {-260} duration={500}>program
+        </Link></li>
+        <li> <Link to='about' smooth ={true} offset= {-150} duration={500}>About Us 
+        </Link></li>
+
+        <li> <Link to='campus' smooth ={true} offset= {-260} duration={500}>Campus
+        </Link></li>
+        <li> <Link to='testimonials' smooth ={true} offset= {-260} duration={500}>Testimonials 
+        </Link></li>
+      <li><Link to ='contact' smooth={true} offset={-260} duration={500} className='btn' >Contact Us</Link></li>
+        
+     
         
       </ul>
     </nav>
